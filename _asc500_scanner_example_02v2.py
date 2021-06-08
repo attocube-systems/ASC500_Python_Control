afa962d91f9946e2b539b5ca47747cae0f1de531
Clemens Schäfermeier <clemens@fh-muenster.de>
2021-06-08 11:46:21 +0200

    Update _asc500_scanner_example_02v2.py
    Still not working

--- _asc500_scanner_example_02v2.py
+++ _asc500_scanner_example_02v2.py
@@ -5,35 +5,28 @@ Created on Mon Jun  7 08:28:42 2021
 @author: grundch
 """
 
-import numpy as np
-from matplotlib import pyplot as plt
 import asc500_base as asc
 import time
-import os
-import ctypes as ct
 
+
+
+# Some Constants ----------------------------------------------------------------
 chNo = 0
 average = 0
 sampTime = 0
 bufSize = 1024
-
-
-# Some Constants ----------------------------------------------------------------
-columns         = 100                                           # Scanrange number of columns
-lines           = 150                                           # Scanrange number of lines
-pxSize       = 50                                          # Width of a column/line [10pm]
-sampTime      = 1                                           # Scanner sample time [2.5us]
-frameSize       = columns*lines*2                               # Amount of data in a frame
+columns = 100                                           # Scanrange number of columns
+lines = 150                                           # Scanrange number of lines
+pxSize = 50                                          # Width of a column/line [10pm]
+sampTime = 1                                           # Scanner sample time [2.5us]
+frameSize = columns *lines * 2                               # Amount of data in a frame
 DYB_EVT_DATA_00 = chNo                                             # from daisydata.h
-DYB_EVT_CUSTOM  = 0x8000                                        # from daisydata.h
+DYB_EVT_CUSTOM = 0x8000                                        # from daisydata.h
 
 def pollDataFull():
-    event   = 0                                                 # Returncode of waitForEvent
-    #frameNo = ct.c_int32( 0 )                                      # Return param of getDataBuffer
-    #index   = ct.c_int32( 0 )                                      # Return param of getDataBuffer
-    #dSize   = ct.c_int32( frameSize )                              # In- and output of getDataBuffer
-    #frame   = (ct.c_int32 * frameSize)()                           # Array to receive data
-    #meta    = (ct.c_int32 * 13)()                                  # Metadata, should be a struct...
+    event = 0                                                 # Returncode of waitForEvent
+
+    #meta = (ct.c_int32 * 13)()                                  # Metadata, should be a struct...
 
     # Wait for full buffer on channel 0 and show progress
     while ( event == 0 ):
@@ -60,12 +53,33 @@ def pollDataFull():
     else:
         raise( "No data have been received!" )
 
+def sendScannerCommand(command):
+    if (command == asc500.getConst('SCANRUN_ON') ):
+        # Scan start requires two commands; the first one to move to the start position,
+        # (which can take a long time), the second one to actually run the scan.
+        # A rather simple approach: send command cyclically until the scanner is running
+        state = 0
+        while ( (state & asc500.getConst('SCANSTATE_SCAN')) == 0 ):
+            asc500.setParameter( asc500.getConst('ID_SCAN_COMMAND'), 0, command )
+            time.sleep( .1 )
+            state = asc500.getParameter( asc500.getConst('ID_SCAN_STATUS'), 0 )
+            print( "Scanner State: ", end='' );
+            if ( state & asc500.getConst('SCANSTATE_PAUSE')  ): print( "Pause ", end='' )
+            if ( state & asc500.getConst('SCANSTATE_MOVING') ): print( "Move ",  end='' )
+            if ( state & asc500.getConst('SCANSTATE_SCAN')   ): print( "Scan ",  end='' )
+            if ( state & asc500.getConst('SCANSTATE_IDLE')   ): print( "Idle ",  end='' )
+            if ( state & asc500.getConst('SCANSTATE_LOOP')   ): print( "Loop ",  end='' )
+            print( "" )
+    else:
+        # Stop and pause only require one command
+        asc.setParameter( asc500.getConst('ID_SCAN_COMMAND'), 0, command )
 
-binPath = "Installer\\ASC500CL-V2.7.6\\"
-dllPath = "64bit_lib\\ASC500CL-LIB-WIN64-V2.7.6\\daisybase\\lib\\"
+
+binPath = 'Installer\\ASC500CL-V2.7.7\\'
+dllPath = '64bit_lib\\ASC500CL-LIB-WIN64-V2.7.7\\daisybase\\lib\\'
 asc500 = asc.ASC500Base(binPath, dllPath)
-asc500.startServer()
-asc500.sendProfile('Installer\\ASC500CL-V2.7.6\\afm.ngp')
+asc500.startServer('FindSim')
+asc500.sendProfile('Installer\\ASC500CL-V2.7.7\\afm.ngp')
 asc500.setDataEnable(1)
 asc500.configureChannel(chNo,
                         asc500.getConst('CHANCONN_SCANNER'),
@@ -76,21 +90,16 @@ print(asc500.getChannelConfig(chNo))
 
 asc500.configureDataBuffering(chNo, bufSize)
 
-#asc500.setCounterExposureTime(expTime)
-#print("Exposure time ", asc500.getCounterExposureTime())
-
-
-
 #config Scanner
-asc500.setParameter( asc500.getConst('ID_SCAN_X_EQ_Y'),   0, 0 )        # Switch off annoying automatics ..
-asc500.setParameter( asc500.getConst('ID_SCAN_GEOMODE'),  0, 0 )        # that are useful only for GUI users
-asc500.setParameter( asc500.getConst('ID_SCAN_PIXEL'),    pxSize, 0 ) # Adjust scanner parameters
-asc500.setParameter( asc500.getConst('ID_SCAN_COLUMNS'),  columns, 0 )
-asc500.setParameter( asc500.getConst('ID_SCAN_LINES'),   lines, 0 )
-asc500.setParameter( asc500.getConst('ID_SCAN_OFFSET_X'), 75 * pxSize, 0 )
-asc500.setParameter( asc500.getConst('ID_SCAN_OFFSET_Y'), 75 * pxSize, 0 )
-asc500.setParameter( asc500.getConst('ID_SCAN_MSPPX'),    sampTime, 0 )
-asc500.setParameter( asc500.getConst('ID_SCAN_ONCE'), 1)
+asc500.setParameter(asc500.getConst('ID_SCAN_X_EQ_Y'), 0, 0) # Switch off annoying automatics ..
+asc500.setParameter(asc500.getConst('ID_SCAN_GEOMODE'), 0, 0)        # that are useful only for GUI users
+asc500.setParameter(asc500.getConst('ID_SCAN_PIXEL'), pxSize, 0) # Adjust scanner parameters
+asc500.setParameter(asc500.getConst('ID_SCAN_COLUMNS'),  columns, 0)
+asc500.setParameter(asc500.getConst('ID_SCAN_LINES'), lines, 0)
+asc500.setParameter(asc500.getConst('ID_SCAN_OFFSET_X'), 75 * pxSize, 0)
+asc500.setParameter(asc500.getConst('ID_SCAN_OFFSET_Y'), 75 * pxSize, 0)
+asc500.setParameter(asc500.getConst('ID_SCAN_MSPPX'), sampTime, 0)
+asc500.setParameter(asc500.getConst('ID_SCAN_ONCE'), 1)
 
 # Enable Outputs and wait for success (enable outputs takes some time)
 outActive = 0
@@ -102,9 +111,9 @@ while ( outActive == 0 ):
 
 out = []
 #start scanning
-asc500.sendScannerCommand( asc500.getConst('SCANRUN_ON') )                      # Start scanner
-out.append(pollDataFull())                                              # Wait for data
-asc500.sendScannerCommand( asc500.getConst('SCANRUN_OFF') )                     # Stop scanner
+sendScannerCommand( asc500.getConst('SCANRUN_ON') ) # Start scanner
+out.append(pollDataFull()) # Wait for data
+sendScannerCommand( asc500.getConst('SCANRUN_OFF') ) # Stop scanner
 
 # Disable Outputs and wait until finished.
 # We use wait for event instead of polling for demonstration.
