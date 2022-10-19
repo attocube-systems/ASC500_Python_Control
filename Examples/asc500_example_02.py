e8dfd839ac7b00f7ca214da15b415a54f577d6fc
Clemens Schäfermeier <clemens@fh-muenster.de>
2022-10-19 17:41:01 +0200

    Added two examples

---
+++ Examples/asc500_example_02.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 13:04:04 2020
+
+@author: schaecl
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import asc500_device as asc
+
+binPath = "..\\Installer\\ASC500CL-V2.7.13\\"
+dllPath = "..\\64bit_lib\\ASC500CL-LIB-WIN64-V2.7.13\\daisybase\\lib\\"
+
+asc500 = asc.Device(binPath, dllPath)
+# asc500 = asc.ASC500Base(binPath, dllPath)
+
+asc500.base.startServer('FindSim')
+
+asc500.base.sendProfile(binPath + '20221019_AFM_Akiyama.ngp')
+
+asc500.data.setDataEnable(1)
+asc500.base.setOutputsWaiting(1)
+sampTime = 1e-3
+average = 0
+chnNo = 0
+bufSize = 256
+expTime = 1e-6 # Counter exposure time in us
+
+asc500.data.configureChannel(chnNo,
+                        asc500.base.getConst('CHANCONN_PERMANENT'),
+                        asc500.base.getConst('CHANADC_AFMAMPL'),
+                        average,
+                        sampTime)
+
+print(asc500.data.getChannelConfig(chnNo))
+
+asc500.data.configureDataBuffering(chnNo, bufSize)
+
+asc500.data.setCounterExposureTime(expTime)
+print("Exposure time ", asc500.data.getCounterExposureTime())
+
+#%% Poll data
+
+while True:
+    # Wait until buffer is full
+    if asc500.data.waitForFullBuffer(chnNo) != 0:
+        break
+
+out = \
+asc500.data.getDataBuffer(chnNo,
+                     0,
+                     bufSize)
+
+#%% Close ASC500
+
+asc500.base.stopServer()
+
+#%% Check data
+
+print("Frame number: ", out[0])
+print("Index       : ", out[1])
+print("Data size   : ", out[2])
+print("Meta data   : ", out[4])
+counts = np.asarray(out[3][:])
+print("Data        :\n", counts)
+
+#%% Plot counts
+
+plt.figure(0)
+
+plt.scatter((np.arange(bufSize) + 1) * 2.5e-6 * expTime * 1e3,
+            counts)
+plt.xlabel('Time / ms')
+plt.ylabel('Counts / 1')
